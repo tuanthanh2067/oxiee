eced8a3594bd1b9a64d6365c6b2e4e5fecdf54f3
tuanthanh2067@gmail.com <tuanthanh2067@gmail.com>
2021-12-22 14:34:39 -0500

    add footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,16 +2,19 @@
   <div class="container">
     <Navbar />
     <router-view></router-view>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
   name: "App",
   components: {
     Navbar,
+    Footer,
   },
 };
 </script>
---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,69 @@
+<template>
+  <footer>
+    <div class="links">
+      <div>
+        <Logo />
+        <p>Company management</p>
+      </div>
+
+      <div class="info">
+        <h3>Information</h3>
+        <router-link to="/contact">Contact us</router-link>
+        <router-link to="/about">About us</router-link>
+      </div>
+
+      <div class="auth">
+        <h3>Authentication</h3>
+        <router-link to="/signup">Sign up</router-link>
+        <router-link to="/login">Log in</router-link>
+      </div>
+    </div>
+    <div class="rights">
+      <p>© 2021 Oxiee. All rights reserved.</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+import Logo from "@/components/Logo.vue";
+
+export default {
+  name: "Footer",
+  components: {
+    Logo,
+  },
+};
+</script>
+
+<style scoped>
+footer {
+  padding: 3em;
+  height: 300px;
+  background: rgb(246, 246, 246);
+}
+
+.links {
+  display: flex;
+  justify-content: space-between;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+.auth,
+.info {
+  display: flex;
+  flex-direction: column;
+}
+
+.rights {
+  margin-top: 5em;
+  border-top: 1px solid rgb(221, 221, 221);
+}
+
+.rights p {
+  margin-top: 1.25em;
+}
+</style>

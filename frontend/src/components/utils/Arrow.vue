ae5b8b97cb100e90c7125f141fd9d8ddceb9d586
tuanthanh2067@gmail.com <tuanthanh2067@gmail.com>
2021-12-27 12:58:03 -0500

    update login view

---
+++ frontend/src/components/utils/Arrow.vue
@@ -0,0 +1,39 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    xmlns="http://www.w3.org/2000/svg"
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    :style="{ fill: color }"
+    :class="orientation"
+  >
+    <path
+      d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "ArrowIcon",
+  props: {
+    orientation: String,
+    color: String,
+  },
+};
+</script>
+
+<style scoped>
+.left {
+  transform: rotate(-180deg);
+}
+
+.top {
+  transform: rotate(-90deg);
+}
+
+.bottom {
+  transform: rotate(90deg);
+}
+</style>
--- frontend/src/views/auth/Login.vue
+++ frontend/src/views/auth/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="login">
-    <div class="left">
+    <div id="left">
       <div class="container">
         <h2>Welcome back</h2>
         <p>Hello, please fill in your credentials</p>
@@ -33,22 +33,67 @@
         </span>
       </div>
     </div>
-    <div class="right">
+    <div id="right">
       <img src="@/assets/login-image.jpg" alt="accounting" />
+
+      <div class="glass">
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+          corrupti ducimus harum ipsum ea omnis commodi totam autem assumenda
+          culpa.
+        </h2>
+
+        <div class="rate">
+          <h1>Lauren K.</h1>
+          <div class="stars">
+            <Stars color="white" />
+            <Stars color="white" />
+            <Stars color="white" />
+            <Stars color="white" />
+          </div>
+        </div>
+
+        <div class="sup">
+          <div class="title">
+            <h4>Restaurant Manager</h4>
+            <p>Big World Hotel</p>
+          </div>
+          <div class="arrows">
+            <div>
+              <Arrow orientation="left" color="white" />
+            </div>
+            <div>
+              <Arrow orientation="right" color="white" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
-<script></script>
+<script>
+import Stars from "@/components/utils/Star.vue";
+import Arrow from "@/components/utils/Arrow.vue";
+
+export default {
+  name: "Login",
+  components: {
+    Stars,
+    Arrow,
+  },
+};
+</script>
 
 <style scoped>
 .login {
   width: 100vw;
   height: 100vh;
   display: flex;
+  background-attachment: fixed;
 }
 
-.left {
+#left {
   flex: 40%;
   display: flex;
   justify-content: center;
@@ -114,13 +159,66 @@ input[type="submit"] {
   color: black;
 }
 
-.right {
+#right {
   flex: 60%;
+  position: relative;
 }
 
-.right img {
+#right img {
   width: 100%;
   height: 100%;
   object-fit: cover;
+}
+
+#right .glass {
+  background: inherit;
+  position: absolute;
+  left: 50%;
+  bottom: 2%;
+  transform: translateX(-50%);
+
+  width: 80%;
+  height: 300px;
+
+  background-color: rgba(255, 255, 255, 0.15);
+  backdrop-filter: blur(15px);
+  box-shadow: 0 0 2px 0 white;
+
+  padding: 1.25em;
+  color: white;
+}
+
+.rate {
+  margin-top: 0.875em;
+  display: flex;
+  justify-content: space-between;
+}
+
+.sup {
+  margin-top: 0.75em;
+  display: flex;
+  justify-content: space-between;
+}
+
+.sup .title p {
+  color: white;
+  font-size: 0.95em;
+}
+
+.arrows {
+  display: flex;
+}
+
+.arrows > div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 50px;
+  width: 50px;
+  border: 1px solid rgba(255, 255, 255, 0.4);
+  border-radius: 50%;
+
+  margin-left: 1em;
 }
 </style>

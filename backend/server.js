1008073336e2e0095de3dabceaf80379f34041b3
tuanthanh2067@gmail.com <tuanthanh2067@gmail.com>
2021-12-29 16:26:58 -0500

    added mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,26 @@
 const { createServer } = require("http");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = require("./app");
 
 const httpServer = createServer(app);
 const PORT = process.env.PORT || 8000;
 
-httpServer.listen(PORT);
+mongoose
+  .connect(process.env.MONGOOSE_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    httpServer.listen(PORT);
+    console.log("connected to server");
+  })
+  .catch((err) => {
+    console.error("unable to start the server", err);
+    process.exit(1);
+  });
+
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
